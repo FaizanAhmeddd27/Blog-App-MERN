1ba5fd61206b6676d5d76c779d3905f1aeba534d
FAIZAN AHMED <faizannn27@gmail.com>
2025-08-09 18:57:26 +0500

    new changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Footer from './components/Footer'
-import { Routes, Route, useLocation } from 'react-router-dom'
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Blogs from './pages/Blogs'
@@ -14,20 +14,14 @@ import Creators from './pages/Creators'
 import Update from './dashboard/Update'
 import { Toaster } from "react-hot-toast";
 import { toastOptions } from '../toastConfig'
-import { Navigate } from 'react-router-dom'
 import Detail from './components/Detail'
 
 const App = () => {
-  const { profile, authenticated } = useAuth();
+  const { authenticated } = useAuth();
   const location = useLocation();
 
-  // Paths where Navbar and Footer should be hidden
   const hideLayout = ['/login', '/register', '/dashboard'];
-
-  // Hide layout if the path starts with any of the hideLayout paths
   const shouldHideLayout = hideLayout.some(path => location.pathname.startsWith(path));
-
-  const { blogs } = useAuth();
 
   return (
     <>
@@ -35,20 +29,24 @@ const App = () => {
 
       <div className="bg-slate-200 min-h-screen">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={authenticated ? <Home /> : <Navigate to="/login" />}
+          />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          
           <Route
             path="/dashboard"
             element={authenticated ? <Dashboard /> : <Navigate to="/login" />}
           />
+          
           <Route path="/creators" element={<Creators />} />
           <Route path="/update/:id" element={<Update />} />
           <Route path="/blog/:id" element={<Detail />} /> 
-
         </Routes>
 
         {!shouldHideLayout && <Footer />}
